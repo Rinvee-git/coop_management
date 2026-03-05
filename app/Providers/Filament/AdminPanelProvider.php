fdcaed3c80f4e05d12bcfb03945607e806c58bcb
ferntzy <rolaniog@gmail.com>
2026-03-05 20:35:50 +0800

    made the website into coop instead of admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('coop')
             ->login()
             ->plugins([
                 FilamentShieldPlugin::make()
